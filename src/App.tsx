7a5cef59d31319d964a84c5d22102400c23c27aa
graftcode <graftcode@outlook.com>
2023-01-06 22:39:28 +0000

    check query in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useQuery } from "@apollo/client";
+
+import { getCharacters } from "./graphql/Queries";
+
 import "./App.css";
 
 const App = (): JSX.Element => {
+  const { data, error, loading } = useQuery(getCharacters);
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <h1>hello world</h1>
     </div>
   );
 };
